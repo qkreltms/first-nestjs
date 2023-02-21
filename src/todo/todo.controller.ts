0146f40382852c1a832ed4ed8aa6068d7b4911d1
junghoonpark <junghoon@lumigloo.com>
2023-02-21 18:54:00 +0900

    update: S3 연동

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -8,10 +8,14 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { Todo } from 'src/todo/todo.entity';
 import { TodoService } from 'src/todo/todo.service';
 import { CheckTodo } from './util';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
 
 @Controller('todo')
 export class TodoController {
@@ -45,5 +49,30 @@ export class TodoController {
     id,
   ) {
     return this.todoService.delete(id);
+  }
+
+  @Post('file')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      },
+    });
+
+    try {
+      const upload = await new AWS.S3()
+        .putObject({
+          Key: `${Date.now() + file.originalname}`,
+          Body: file.buffer,
+          Bucket: process.env.AWS_BUCKET_NAME,
+        })
+        .promise();
+      console.log(upload);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
